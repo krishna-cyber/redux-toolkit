117f239ec54cde29895d4b394a2a46e24777a3cf
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-09 12:24:05 +0545

    author based post system

--- post store/src/features/AddForm.jsx
+++ post store/src/features/AddForm.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { nanoid } from "@reduxjs/toolkit";
+import { postAdded } from "./posts/postSlice";
+import { useDispatch } from "react-redux";
 
 const AddForm = () => {
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
-    watch,
+    reset,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const savepost = (data) => {
+    dispatch(postAdded(data.title, data.content));
+    reset();
+  };
   return (
     <div>
       <h2>Add New posts</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(savepost)}>
         <label htmlFor='title'>Title:</label>
         <input {...register("title")} />
         <label htmlFor='content'>Content:</label>
         <textarea {...register("content", { required: true })} />
 
         {errors.exampleRequired && <span>This field is required</span>}
-
+        <option value=''>Select a category</option>
         <input type='submit' />
       </form>
     </div>
