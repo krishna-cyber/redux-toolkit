5a7aa4ceb5a96c65eebca8d9e1a3144ac1130d07
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-12 14:56:20 +0545

    post exverpt file and logic witten

--- redux-thunk writing async code/src/features/posts/PostExcerpt.jsx
+++ redux-thunk writing async code/src/features/posts/PostExcerpt.jsx
@@ -3,7 +3,20 @@ import ReactionButtons from "./ReactionButtons";
 import PostAuthor from "./PostAuthor";
 
 const PostExcerpt = ({ post }) => {
-  return <div></div>;
+  return (
+    <>
+      {orderedPosts.map((post) => (
+        <article className=' border border-blue-800 p-2 rounded-lg '>
+          <h3 className=' font-bold text-lg'>{post.title}</h3>
+          <p className=' text-slate-800 text-lg'>{post.content}</p>
+          <PostAuthor userID={post.user} />
+          <TimeAgo timestamp={post.date} />
+
+          <ReactionButtons post={post} />
+        </article>
+      ))}
+    </>
+  );
 };
 
 export default PostExcerpt;
